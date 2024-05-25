7f1058bcffbc76a544d77278c847f26d50888fb8
Maxey <92372504+Maxey1950@users.noreply.github.com>
2024-05-24 21:33:03 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/Maxey1950/2f8c9f88af6cbcb6c4bde58a20bc93eb/raw/a350fc3efbc767c3408ecc5f19e5c629fed85743/gistfile1.txt",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TrollCodes/Roblox-Extension-Code-Files/main/apiti"))()
