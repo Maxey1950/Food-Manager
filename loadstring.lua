0ea5b9d147c827855095b03d06a83391802ecd9b
Maxey <92372504+Maxey1950@users.noreply.github.com>
2024-05-24 21:29:40 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/Maxey1950/975fd0b33dd3f1c5d3043861caed304b/raw/65ae10d87f08baa141f15306212b55e4d71483fe/gistfile1.txt",true))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/Maxey1950/2f8c9f88af6cbcb6c4bde58a20bc93eb/raw/a350fc3efbc767c3408ecc5f19e5c629fed85743/gistfile1.txt",true))()
